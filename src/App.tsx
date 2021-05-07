eb7e5f53df427f201970199c67ff6e0b5da42f13
sprushika <sprushika1993@gmail.com>
2021-05-07 19:36:22 +0530

    code refactor
    Change-Id: Id524ec7bc114b8ba5982080862756b6ee209e638

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-import styled from "styled-components";
-import { Route, BrowserRouter as Router, Link } from "react-router-dom";
-import { menuList } from "./constants/Constant";
 import NavBar from "./components/NavBar/NavBar";
 
 import "./App.css";
