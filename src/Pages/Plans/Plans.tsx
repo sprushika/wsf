cf9cf346b6e7ca79e4f0de801cbbbf8a6def01be
Sharatherrabelly <83690002+Sharatherrabelly@users.noreply.github.com>
2021-05-20 09:32:34 +0530

    Refracted Plans and Plans Style components
    Refracted Plans and Plans Style components

--- src/Pages/Plans/Plans.tsx
+++ src/Pages/Plans/Plans.tsx
@@ -1,10 +1,68 @@
-import React from "react";
-import { CenterDiv } from "./PlansElements";
+import React, { useState } from 'react';
+import {
+	Header,
+	PlansContainerDiv,
+	PlanDiv,
+	PlanBilling,
+	PlanOverViewDiv,
+	PlanDetailsDiv,
+	CenteredBtn
+} from './PlansElements';
+import { plansList } from '../../Global/Constant';
+const Plans = () => {
+	const [ data, setPlansList ] = useState(plansList);
+	const handleClick = (index: number) => {
+		debugger;
+		let newPlansList = [ ...data ];
+		newPlansList.forEach((plan) => {
+			plan.isactive = false;
+		});
+		newPlansList[index].isactive = true;
+		setPlansList(newPlansList);
+	};
+	const showResgisterBtn = (index: number) => {
+		let newPlansList = [ ...data ];
+		newPlansList[index].showHideBtn = true;
+		setPlansList(newPlansList);
+	};
+	const hideResgisterBtn = (index: number) => {
+		let newPlansList = [ ...data ];
+		newPlansList[index].showHideBtn = false;
+		setPlansList(newPlansList);
+	};
 
-const Plans = ()=> {
-  return(
-    <CenterDiv><h1>Plans</h1></CenterDiv>
-  )
-}
+	return (
+		<div>
+			<div>
+				<Header>Our Pricing Model</Header>
+			</div>
+
+			<PlansContainerDiv>
+				{plansList.map((plan, index) => (
+					<PlanDiv
+						key={plan.Title}
+						active={plan.isactive}
+						onClick={(e) => handleClick(index)}
+						onMouseEnter={(e) => showResgisterBtn(index)}
+						onMouseLeave={(e) => hideResgisterBtn(index)}>
+						<PlanOverViewDiv style={{ backgroundColor: plan.backgroundColor }}>
+							<p>{plan.Title}</p>
+							<p>{plan.planOverview}</p>
+							<PlanBilling>
+								<span>{plan.planCurrency}</span>
+								<span>{plan.planAmount}</span>
+							</PlanBilling>
+							<PlanBilling>{plan.planBilling}</PlanBilling>
+						</PlanOverViewDiv>
+						<PlanDetailsDiv>
+							{plan.planBenefits.map((planbenefit) => <p>{planbenefit}</p>)}
+							{plan.showHideBtn ? <CenteredBtn type="submit" text="Register" allowDisabledFocus /> : null}
+						</PlanDetailsDiv>
+					</PlanDiv>
+				))}
+			</PlansContainerDiv>
+		</div>
+	);
+};
 
 export default Plans;
--- src/Pages/Plans/PlansElements.ts
+++ src/Pages/Plans/PlansElements.ts
@@ -1,8 +1,81 @@
-import styled from 'styled-components';
 
-export const CenterDiv = styled.div`
-/* display: flex;
-justify-content: center;
-align-items: center;
-font-size: 1rem; */
+import styled from 'styled-components';
+import { PrimaryButton } from '@fluentui/react';
+interface IProps {
+    active?: boolean;
+};
+
+export const Header = styled.h1`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
 `
+export const PlansContainerDiv = styled.div`
+    display: flex;
+    gap: 60px;
+` 
+export const PlanDiv = styled.div`
+    position: ${(props:IProps) => props.active  ? `relative;
+    ` : 'auto'};
+    top: ${(props:IProps) => props.active  ? `-20px;
+    ` : '0'};
+    box-shadow:2px 2px 4px rgb(0 0 0 / 40%),-2px -2px 4px rgb(0 0 0 / 40%);
+    background-color: #f1f1f1;
+    height:550px;
+    border-radius:25px;
+`
+export const PlanOverViewDiv=styled.div`
+    padding: 10px;
+    border-top-left-radius: 25px;
+    border-top-right-radius: 25px;
+    text-align:center;
+        > p:nth-child(1)
+        {
+            margin-top: 30px;
+            font-size: 18px;
+            font-weight: 600;
+        }
+        > p:nth-child(2)
+        {
+            font-size: 13px !important;
+        }
+`
+export const PlanBilling = styled.p`
+    text-align:center;
+    font-size: 12px;
+        > span:nth-child(1) {
+            font-size: 1em !important;
+            vertical-align: top;
+        } 
+        > span:nth-child(2){
+            font-size: 35px !important; 
+            line-height: 25px;
+        }
+` 
+export const PlanDetailsDiv=styled.span`
+    background-color: #f1f1f1;
+    height: 550px;
+    border-radius: 25px;
+    text-align: center;
+        > p:nth-child(1)
+        {
+            font-size: 16px;
+            font-weight: 600;
+        }
+        > p:nth-child(2)
+        {
+            font-size: 14px;
+            
+        }
+`
+
+export const CenteredBtn=styled(PrimaryButton)`
+    margin-left: 80px
+`
+
+
+
+
+
+
+
